97193bde6cd12fde4f7d8ba772cce46edfbc3a88
Henrique Hoffman Câmara <hoffman.camara@hotmail.com>
2025-10-25 13:41:23 -0300

    fix the LTN to LTK

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,6 +1,6 @@
 public static class Constants
 {
-    public const string BASE_PATH = "LTN/";
+    public const string BASE_PATH = "LTK/";
     public const string BASE_CREATE_MODIFIER_PATH = BASE_PATH + "Modifiers/";
     public const string BASE_CREATE_DATA_PATH = BASE_PATH + "Data/";
     public const string BASE_CREATE_ACTIONS_PATH = BASE_PATH + "Actions/";
