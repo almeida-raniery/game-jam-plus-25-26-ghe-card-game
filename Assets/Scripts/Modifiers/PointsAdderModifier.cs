11622afe65a5551800126b618b56502166118c4d
Henrique Hoffman Câmara <hoffman.camara@hotmail.com>
2025-10-25 11:52:03 -0300

    Initial modifiers implementation

---
+++ Assets/Scripts/Modifiers/PointsAdderModifier.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "PointsAdderModifier", menuName = Constants.BASE_CREATE_MODIFIER_PATH + "Points Adder Modifier")]
+public class PointsAdderModifier : ModifierBase
+{
+    public override void Modify()
+    {
+    }
+}
---
+++ Assets/Scripts/Modifiers/PointsMultiplierModifier.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+[CreateAssetMenu(fileName = "PointsMultiplierModifier", menuName = Constants.BASE_CREATE_MODIFIER_PATH + "Points Multiplier Modifier")]
+public class PointsMultiplierModifier : ModifierBase
+{
+    public override void Modify()
+    {
+    }
+}
