9a7eb59c1d5207f6aa4ec8317d7637efabd2d872
Henrique Hoffman Câmara <hoffman.camara@hotmail.com>
2025-10-26 17:48:57 -0300

    Resources show

--- Assets/Scripts/Cards/CardActions/CardAction.cs
+++ Assets/Scripts/Cards/CardActions/CardAction.cs
@@ -4,6 +4,7 @@ public abstract class CardAction : ScriptableObject
 {
     public string CardActionName;
     public string CardActionDescription;
+    public GiveCardAction GiveCardAction;
 
     public abstract void ExecuteAction();
 }
--- Assets/Scripts/Presenters/CardPresenter.cs
+++ Assets/Scripts/Presenters/CardPresenter.cs
@@ -10,9 +10,27 @@ public class CardPresenter : MonoBehaviour
     [SerializeField] Image cardImage;
     [SerializeField] TextMeshProUGUI cardLeftActionText;
     [SerializeField] TextMeshProUGUI cardRightActionText;
-    
+
+    [Header("Left Action")]
+    public TextMeshProUGUI leftActionFloraGainText;
+    public TextMeshProUGUI leftActionFaunaGainText;
+    public TextMeshProUGUI leftActionCultureGainText;
+    public TextMeshProUGUI leftModifierGainText;
+    public TextMeshProUGUI leftCardGainText;
+
+    [Header("Right Action")]
+    public TextMeshProUGUI rightActionFloraGainText;
+    public TextMeshProUGUI rightActionFaunaGainText;
+    public TextMeshProUGUI rightActionCultureGainText;
+    public TextMeshProUGUI rightModifierGainText;
+    public TextMeshProUGUI rightCardGainText;
+
     CardAction cardLeftAction;
     CardAction cardRightAction;
+
+    [SerializeField] ResourceData faunaResource;
+    [SerializeField] ResourceData floraResource;
+    [SerializeField] ResourceData cultureResource;
 
     private void Awake()
     {
@@ -33,14 +51,118 @@ public class CardPresenter : MonoBehaviour
         cardLeftActionText.text = cardData.cardLeftAction.CardActionName;
         cardRightAction = cardData.cardRightAction;
         cardRightActionText.text = cardData.cardRightAction.CardActionName;
+
+        // set the left resources gain
+        if (cardLeftAction.GiveCardAction != null)
+        {
+            foreach (ResourceValueStruct resval in cardLeftAction.GiveCardAction.resources)
+            {
+                if (resval.resource == faunaResource)
+                {
+                    leftActionFloraGainText.text = getAddSymbol(resval.valueToGain);
+                }
+                else if (resval.resource == floraResource)
+                {
+                    leftActionFaunaGainText.text = getAddSymbol(resval.valueToGain);
+                }
+                else if (resval.resource == cultureResource)
+                {
+                    leftActionCultureGainText.text = getAddSymbol(resval.valueToGain);
+                }
+            }
+
+            leftModifierGainText.text = "";
+
+            if (cardLeftAction.GiveCardAction.modifierToGain) 
+            {
+                leftModifierGainText.text = "+ Mod";
+            }
+            if (cardLeftAction.GiveCardAction.removeRandomModifier)
+            {
+                leftModifierGainText.text = "- Mod";
+            }
+            leftCardGainText.text = "";
+            if (cardLeftAction.GiveCardAction.cardsToGain > 0)
+            {
+                leftCardGainText.text = "+ " + cardLeftAction.GiveCardAction.cardsToGain + " cards";
+            }
+        }
+
+        // set the left resources gain
+        if (cardRightAction.GiveCardAction != null)
+        {
+            foreach (ResourceValueStruct resval in cardRightAction.GiveCardAction.resources)
+            {
+                if (resval.resource == faunaResource)
+                {
+                    rightActionFloraGainText.text = getAddSymbol(resval.valueToGain);
+                }
+                else if (resval.resource == floraResource)
+                {
+                    rightActionFaunaGainText.text = getAddSymbol(resval.valueToGain);
+                }
+                else if (resval.resource == cultureResource)
+                {
+                    rightActionCultureGainText.text = getAddSymbol(resval.valueToGain);
+                }
+            }
+
+            rightModifierGainText.text = "";
+
+            if (cardRightAction.GiveCardAction.modifierToGain)
+            {
+                rightModifierGainText.text = "+ Mod";
+            }
+            if (cardRightAction.GiveCardAction.removeRandomModifier)
+            {
+                rightModifierGainText.text = "- Mod";
+            }
+            rightCardGainText.text = "";
+            if (cardRightAction.GiveCardAction.cardsToGain > 0)
+            {
+                rightCardGainText.text = "+ " + cardRightAction.GiveCardAction.cardsToGain + " cards";
+            }
+        }
+
+        // set the right resources gain
     }
 
-    public void LeftActionClicked() 
+    private string getAddSymbol(int quant)
+    {
+        string returnSymbol = string.Empty;
+        if (quant == 1)
+        {
+            returnSymbol = "+";
+        }
+        else if (quant == 2)
+        {
+            returnSymbol = "++";
+        }
+        else if (quant >= 3)
+        {
+            returnSymbol = "+++";
+        }
+        else if (quant == -1)
+        {
+            returnSymbol = "-";
+        }
+        else if (quant == -2)
+        {
+            returnSymbol = "--";
+        }
+        else if (quant <= -3)
+        {
+            returnSymbol = "---";
+        }
+        return returnSymbol;
+    }
+
+    public void LeftActionClicked()
     {
         EventBus.CardActionChoosenEvent(cardLeftAction);
     }
 
-    public void RightActionClicked() 
+    public void RightActionClicked()
     {
         EventBus.CardActionChoosenEvent(cardRightAction);
     }
